af3d9950494ecd0590c3e7d7a2f568814e41c3c1
Pawel Szostek <pawel.szostek@gmail.com>
2012-01-02 22:11:50 +0100

    Move date conversion to Sanitizer

--- newsgenie/sanitizer.py
+++ newsgenie/sanitizer.py
@@ -22,3 +22,30 @@ class Sanitizer(object):
             else:
                 ret += char
         return ret
+
+    def _calculate_date_offset(self, rss_date):
+        if "GMT" in rss_date:
+            rss_date = rss_date.rstrip("GMT")
+            rss_date = rss_date.strip()
+            return (rss_date,0)
+        else:
+            offset = rss_date[-5:]
+            print rss_date
+            rss_date = rss_date[:-5]
+            rss_date = rss_date.strip()
+            if offset[0] == "-":
+                return (rss_date,-3600*int(offset[1]))
+            else:
+                return (rss_date, 3600*int(offset[1]))
+
+    def convert_to_timestamp(self, rss_date):
+        if rss_date == '':
+            from time import time
+            return int(time())
+        else:
+            from time import mktime, strptime
+            rss_date, offset = self._calculate_date_offset(rss_date)
+            tf = "%a, %d %b %Y %H:%M:%S"
+            ts = mktime(strptime(rss_date, tf))
+            ts = int(ts) + offset
+            return ts
